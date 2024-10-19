94302ccfbed90fe7262faedcd46d2540c673338d
Sven Rahn <apps.randombyte@gmail.com>
2024-10-19 17:19:42 +0200

    Redesign card

--- src/components/Person.svelte
+++ src/components/Person.svelte
@@ -4,69 +4,70 @@
 	import type { Person } from "$lib/server/database";
 	import IconButton, { Icon } from "@smui/icon-button";
 	import { mdiDelete, mdiPlus } from "@mdi/js";
-	import Card, { Content, Actions } from "@smui/card";
-	import Button, { Label } from "@smui/button";
 	import Expense from "./Expense.svelte";
+	import Paper, { Title, Subtitle, Content } from "@smui/paper";
 
 	export let form;
 	export let person: Person;
 </script>
 
 <div id="container">
-	<Card style="width: 100%">
-		<Content>
-			<div id="header">
-				<p>{person.name}</p>
-			</div>
-
-			<div id="list">
-				{#each person.expenses as expense}
-					<form
-						method="POST"
-						action="?/deleteExpense"
-						use:enhance
-						style="align-self: center"
-						class="row"
-					>
-						<input type="hidden" name={constants.FORM_PERSON_ID} value={person.id} />
-						<input type="hidden" name={constants.FORM_EXPENSE_ID} value={expense?.id} />
-
-						<Expense name={expense.name} amountCents={expense.amountCents} />
-
-						<IconButton>
-							<Icon tag="svg">
-								<path d={mdiDelete} />
-							</Icon>
-						</IconButton>
-					</form>
-				{/each}
-			</div>
-
-			<form method="POST" action="?/createExpense" use:enhance class="row">
+	<Paper style="width: 100%; padding: 0px">
+		<div id="header">
+			<h3 style="padding-left: 16px">{person.name}</h3>
+			<form method="POST" action="?/deletePerson" use:enhance style="align-self:center">
 				<input type="hidden" name={constants.FORM_PERSON_ID} value={person.id} />
-
-				<Expense />
-
 				<IconButton>
 					<Icon tag="svg">
-						<path d={mdiPlus} />
+						<path d={mdiDelete} />
 					</Icon>
 				</IconButton>
 			</form>
+		</div>
 
-			{#if form?.error && form?.personId == person.id}
-				<p>Error: {form.error}</p>
-			{/if}
-		</Content>
-		<Actions style="justify-content: end">
-			<form method="POST" action="?/deletePerson" use:enhance>
-				<input type="hidden" name={constants.FORM_PERSON_ID} value={person.id} />
-				<Button>
-					<Label>Delete</Label>
-				</Button>
-			</form>
-		</Actions>
-	</Card>
+		<div style="padding: 24px;">
+			<Content>
+				<div id="list">
+					{#each person.expenses as expense}
+						<form
+							method="POST"
+							action="?/deleteExpense"
+							use:enhance
+							style="align-self: center"
+							class="row"
+						>
+							<input type="hidden" name={constants.FORM_PERSON_ID} value={person.id} />
+							<input type="hidden" name={constants.FORM_EXPENSE_ID} value={expense?.id} />
+
+							<Expense name={expense.name} amountCents={expense.amountCents} />
+
+							<IconButton>
+								<Icon tag="svg">
+									<path d={mdiDelete} />
+								</Icon>
+							</IconButton>
+						</form>
+					{/each}
+				</div>
+
+				<form method="POST" action="?/createExpense" use:enhance class="row">
+					<input type="hidden" name={constants.FORM_PERSON_ID} value={person.id} />
+
+					<Expense />
+
+					<IconButton>
+						<Icon tag="svg">
+							<path d={mdiPlus} />
+						</Icon>
+					</IconButton>
+				</form>
+
+				{#if form?.error && form?.personId == person.id}
+					<p>Error: {form.error}</p>
+				{/if}
+			</Content>
+		</div>
+	</Paper>
 </div>
 
 <style>
@@ -80,6 +81,8 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
+		background-color: lightgray;
+		border-radius: 4px 4px 0px 0px;
 	}
 
 	#list {
